68072a1da99d9bd3e2da4315b6f00492cc98b862
Iupac120 <oforokpara@gmail.com>
2023-10-12 15:03:59 +0100

    First commit on function

---
+++ app.js
@@ -0,0 +1,18 @@
+
+
+function add (){ ///function declaration
+
+}
+
+const add = function (){ // function expression
+
+}
+
+
+const addNo = () => {  //arrow function
+
+}
+
+console.log("name",addNo)
+
+function addTwo ()
